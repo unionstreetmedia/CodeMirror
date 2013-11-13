7db8ef71e0917d34d91f347e957c18281c1dfd43
Marijn Haverbeke <marijnh@gmail.com>
2013-11-13 22:42:09 +0100

    Wrap all modules in AMD and CommonJS-supporting shims

--- addon/wrap/hardwrap.js
+++ addon/wrap/hardwrap.js
@@ -1,4 +1,11 @@
-(function() {
+(function(mod) {
+  if (typeof exports == "object" && typeof module == "object") // CommonJS
+    mod(require("../../lib/codemirror"));
+  else if (typeof define == "function" && define.amd) // AMD
+    define(["../../lib/codemirror"], mod);
+  else // Plain browser env
+    mod(CodeMirror);
+})(function(CodeMirror) {
   "use strict";
 
   var Pos = CodeMirror.Pos;
@@ -104,4 +111,4 @@
         wrapRange(cm, Pos(paras[i].from, 0), Pos(paras[i].to - 1), options);
     });
   });
-})();
+});
